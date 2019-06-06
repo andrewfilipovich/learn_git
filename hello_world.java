d13a6c47e36aa80b3cc302956c9bcc47e0108fb2
Andrew Filipovich <iamandreyfilip@gmail.com>
2019-06-06 14:00:32 +0300

    swap line into file hello_world

--- hello_world.java
+++ hello_world.java
@@ -3,7 +3,7 @@ class hello_world
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
-		System.out.println("Goodbye!");
 		System.out.println("Have a good one, developer;).");
+		System.out.println("Goodbye!");
 	}
 }
