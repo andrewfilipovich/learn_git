b4d82108d89f9948e79b54a70c5552d1f68aa242
Andrew Filipovich <iamandreyfilip@gmail.com>
2019-06-06 10:29:30 +0300

    add files form local repositorie

---
+++ hello_world.java
@@ -0,0 +1,9 @@
+class hello_world
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+		System.out.println("Goodbye!");
+		System.out.println("Have a good one, developer;).");
+	}
+}
