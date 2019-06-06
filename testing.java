d1dcbd32c07b0aa6382b56bba33f5e87b558f4a1
Andrew Filipovich <iamandreyfilip@gmail.com>
2019-06-06 14:42:36 +0300

    change file -- delete println

--- testing.java
+++ testing.java
@@ -2,8 +2,7 @@ class testing
 {
 	public static void main(String args[])
 	{
-		System.out.println("I want to say such word as ");
-		System.out.println("TEST");
-		System.out.println("Oh, OMG, I forgot to say "Hello".( Sorry, my friend.");
+		System.out.println("I want to say such word as TEST");
+		System.out.println("OMG, I forgot to say Hello. Sorry.");
 	}
 }
