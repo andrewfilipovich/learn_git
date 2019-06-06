986a4cdbdb3b7445b1bcb3697757254cc7f89332
Andrew Filipovich <iamandreyfilip@gmail.com>
2019-06-06 15:31:38 +0300

    upper m and g

--- testing.java
+++ testing.java
@@ -3,7 +3,7 @@ class testing
 	public static void main(String args[])
 	{
 		System.out.println("I want to say such word as TEST");
-		System.out.println("OMG, I forgot to say Hello, sorry.");
+		System.out.println("Omg, I forgot to say Hello, sorry.");
 		System.out.println("TEST");
 	}
 }
