85ea692287b13bf2c8a588feced4faf3ca573d5b
andrewfilipovich <51434729+andrewfilipovich@users.noreply.github.com>
2019-06-06 15:15:44 +0300

    delete line with text goodbye

--- hello_world.java
+++ hello_world.java
@@ -3,7 +3,6 @@ class hello_world
 	public static void main(String args[])
 	{
 		System.out.println("Hello World!");
-		System.out.println("Goodbye!");
 		System.out.println("Have a good one, developer;).");
 	}
 }
